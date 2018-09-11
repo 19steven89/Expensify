872cc65332808fcb069eeec0efd0d082fe549cb3
Steven Barry SE2015 <steven.barry.2016@uni.strath.ac.uk>
2018-09-11 19:55:43 +0100

    added link and navlinks to the page navigation and the error 404 page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// imported exports from the react-router-dom npm 
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -30,12 +31,24 @@ const AddExpensePage = () => (
 
   const NotFoundPage = () => (
     <div>
-      404!
+      404 - <Link to="/">Go Home</Link>
     </div>
+  );
+
+  const Header = () => (
+    <header>
+      <h1>Expensify</h1>
+      <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink><br/>
+      <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink><br/>
+      <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink><br/>
+      <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    </header>
   );
 
 const routes = (
     <BrowserRouter>
+    <div>
+      <Header />
       <Switch>
         <Route path="/" component={ExpenseDashboardPage} exact={true} />
         <Route path="/create" component={AddExpensePage}/>
@@ -43,6 +56,7 @@ const routes = (
         <Route path="/help" component={HelpPage}/>
         <Route component={NotFoundPage}/>
       </Switch>
+    </div>
     </BrowserRouter>
   );
   
