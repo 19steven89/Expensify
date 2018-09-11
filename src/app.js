07a9fe382afcf33137a005fe9c09b0dc2a25d5a3
Steven Barry SE2015 <steven.barry.2016@uni.strath.ac.uk>
2018-09-11 12:16:02 +0100

    set up template for expensify app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 //installed npm validator, now imported it for use
 import React from "react";
 import ReactDOM from "react-dom";
-import IndecisionApp from "./components/IndecisionApp.js";
-// imported node module: module name followed by the filename
+
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
+ReactDOM.render(<p>This is a boilerplate</p>, document.getElementById("app"));
 
 
 
